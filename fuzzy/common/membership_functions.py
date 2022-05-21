d4eeebc507c6c49452f8ae9dd6b6d14d43153638
John Wesley Hostetter <jwhostet@ncsu.edu>
2022-05-21 15:42:42 -0400

    Add PyTorch implementation and tests of Gaussian membership function

--- fuzzy/common/membership_functions.py
+++ fuzzy/common/membership_functions.py
@@ -6,8 +6,59 @@ Created on Wed Nov  3 20:13:35 2021
 @author: john
 """
 
-import numpy as np
+import torch
+import torch.nn as nn
+from torch.nn.parameter import Parameter
 
 
-def gaussian(x, center, sigma):
-    return np.exp(-1.0 * (np.power(x - center, 2) / np.power(sigma, 2)))
+class Gaussian(nn.Module):
+    """
+    Implementation of the Gaussian membership function.
+    Shape:
+        - Input: (N, *) where * means, any number of additional
+          dimensions
+        - Output: (N, *), same shape as the input
+    Parameters:
+        - centers: trainable parameter
+        - sigmas: trainable parameter
+    Examples:
+        # >>> a1 = gaussian(256)
+        # >>> x = torch.randn(256)
+        # >>> x = a1(x)
+    """
+
+    def __init__(self, in_features, centers=None, sigmas=None):
+        """
+        Initialization.
+        INPUT:
+            - in_features: shape of the input
+            - centers: trainable parameter
+            - sigmas: trainable parameter
+            centers and sigmas are initialized randomly by default,
+            but sigmas must be > 0
+        """
+        super(Gaussian, self).__init__()
+        self.in_features = in_features
+
+        # initialize centers
+        if centers is None:
+            self.centers = Parameter(torch.randn(self.in_features))
+        else:
+            self.centers = Parameter(torch.tensor(centers))
+
+        # initialize sigmas
+        if sigmas is None:
+            self.sigmas = Parameter(torch.abs(torch.randn(self.in_features)))
+        else:
+            self.sigmas = Parameter(torch.abs(torch.tensor(sigmas)))
+
+        self.centers.requires_grad = True
+        self.sigmas.requiresGrad = True
+
+    def forward(self, x):
+        """
+        Forward pass of the function.
+        Applies the function to the input elementwise.
+        """
+        self.sigmas = Parameter(torch.abs(self.sigmas))  # constrain the sigma values to only be positive
+        return torch.exp(-1.0 * (torch.pow(x - self.centers, 2) / torch.pow(self.sigmas, 2)))
